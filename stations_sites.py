20fd71d2c33cef4005da1ea556cee7aab7187f12
pedevineau <pierre-etienne.devineau@data.gouv.fr>
2022-05-27 15:08:23 +0200

    Display all States's properties

--- stations_sites.py
+++ stations_sites.py
@@ -17,6 +17,14 @@ DEPARTEMENTS = np.concatenate(
     )
 )
 
+
+def geocode_state_properties():
+    # for each departement, save a csv with State's buildings
+    # curl request to  API Adresse: https://adresse.data.gouv.fr/api-doc/adresse
+    # save curl response in PATH_TO_DEPARTEMENT_GEOCODED_CSV
+    pass
+
+
 def add_geometry(threshold: float = 0) -> None:
     """
     Geocode json list of State's properties and output the .geojson file.
@@ -32,7 +40,7 @@ def add_geometry(threshold: float = 0) -> None:
 
     for dep in DEPARTEMENTS:
         # the departement buildings must have been previously geocoded with API ADRESSE : https://adresse.data.gouv.fr/api-doc/adresse
-        coordinates_dict[dep] = pd.read_csv(f"GEOCODED_{dep}.csv", index_col=0)[
+        coordinates_dict[dep] = pd.read_csv(f"PATH_TO_DEPARTEMENT_GEOCODED_CSV", index_col=0)[
             ["latitude", "longitude", "result_score"]
         ]
         coordinates_dict[dep].rename(
@@ -52,7 +60,7 @@ def add_geometry(threshold: float = 0) -> None:
 
     print("GEOBUILDINGS")
     print(geobuildings.head())
-    geobuildings.to_file("inventaire-immobilier-de-letat_geocoded.geojson")
+    geobuildings.to_file(f"inventaire-immobilier-de-letat_threshold{str(threshold)}.geojson")
     return
 
 
@@ -71,14 +79,14 @@ def load_stations_coordinates(departement: str = "ALL") -> List[List[float]]:
     return [[p.y, p.x] for p in stations.geometry]
 
 
-def load_candidate_site_coordinates(departement: str = "ALL") -> List[List[float]]:
+def load_sites_coordinates(source: str, departement: str = "ALL") -> List[List[float]]:
     """
     The files here is basically a geocoding of the dictionary of low_coverage_sites that we have previously screened
     """
     coords = []
-    sites = gpd.read_file("ALREADY_SAVED_INTERESTING_SITES.geojson")
-  #  if departement in [str(k) for k in range(1, 10)]:
-  #      departement = "0" + departement
+    sites = gpd.read_file(source)
+    #  if departement in [str(k) for k in range(1, 10)]:
+    #      departement = "0" + departement
     if departement.upper() != "ALL":
         sites = sites[sites.dept == str(departement).upper()]
     for _, p in sites.iterrows():
@@ -87,18 +95,18 @@ def load_candidate_site_coordinates(departement: str = "ALL") -> List[List[float
     return coords
 
 
-def load_candidate_addresses(departement: str = "ALL") -> List[str]:
+def load_sites_addresses(source: str, departement: str = "ALL") -> List[str]:
     """
     The files here is basically a geocoding of the dictionary of low_coverage_sites that we have previously screened
-    Aligned with ``load_candidate_site_coordinates``
+    Aligned with ``load_sites_coordinates``
 
     Returns:
         Lits[str]: list of addresses text fields combining city names, street names and building function
     """
     addresses = []
-    sites = gpd.read_file("ALREADY_SAVED_INTERESTING_SITES.geojson")
-   # if departement in [str(k) for k in range(1, 10)]:
-   #     departement = "0" + departement
+    sites = gpd.read_file(source)
+    # if departement in [str(k) for k in range(1, 10)]:
+    #     departement = "0" + departement
     if departement.upper() != "ALL":
         sites = sites[sites.dept == str(departement).upper()]
     for _, p in sites.iterrows():
@@ -129,18 +137,19 @@ def geocode_interesting_sites() -> None:
     major_interest.to_file("ALREADY_SAVED_INTERESTING_SITES.geojson")
 
 
-def save_map(departement: str = "ALL") -> None:
+def save_map(departement: str = "ALL", display_other_properties: bool = False) -> None:
     """
-    Save a .html where ticks with stations are put on open street view
+    Save a .html where ticks with stations, site candidates and (optional) all sites are put on open street map view
 
     Args:
-        departement (str): number of the departement, as a string. Examples: '06' (Nice), '2B' (Haute-Corse), '75' (Paris), or "ALL" for whole France
+        departement (str, optional): id of the departement, as a string.
+            Examples: '06' (Nice), '2B' (Haute-Corse), '75' (Paris), or "ALL" for whole France. Defaults to "ALL".
+        display_other_properties (bool, optional): if True, also display the State's properties that are not considered as good candidates.
+            Defaults to False.
     """
     site_coords = load_stations_coordinates(departement)
     if not site_coords:
         return
-    cand_coords = load_candidate_site_coordinates(departement)
-    cand_towns = load_candidate_addresses(departement)
     if departement.upper() == "ALL":
         m = folium.Map(location=[48.86, 2.33])
     else:
@@ -154,6 +163,26 @@ def save_map(departement: str = "ALL") -> None:
             icon=folium.Icon(color="red"),
             tooltip=tooltip,
         ).add_to(m)
+
+    if display_other_properties:
+        coords = load_sites_coordinates(
+            "inventaire-immobilier-de-letat_threshold0.4.geojson", departement
+        )
+        towns = load_sites_addresses(
+            "inventaire-immobilier-de-letat_threshold0.4.geojson", departement
+        )
+        for k, coord in enumerate(coords):
+            folium.Marker(
+                coord,
+                popup=f"<i>{towns[k]}</i>",
+                icon=folium.Icon(color="blue"),
+                tooltip=tooltip,
+            ).add_to(m)
+
+    # candidate sites are included in all sites
+    cand_file = "ALREADY_SAVED_INTERESTING_SITES.geojson"
+    cand_coords = load_sites_coordinates(cand_file, departement)
+    cand_towns = load_sites_addresses(cand_file, departement)
     for k, coord in enumerate(cand_coords):
         folium.Marker(
             coord,
@@ -161,9 +190,11 @@ def save_map(departement: str = "ALL") -> None:
             icon=folium.Icon(color="orange"),
             tooltip=tooltip,
         ).add_to(m)
+
     m.save(f"index_{departement}.html")
 
+
 if __name__ == "__main__":
-    save_map("ALL")
-  #  for dep in DEPARTEMENTS:
-  #      save_map(str(dep))
+ #   add_geometry(0.4)
+    for dep in DEPARTEMENTS:
+        save_map(str(dep), True)
